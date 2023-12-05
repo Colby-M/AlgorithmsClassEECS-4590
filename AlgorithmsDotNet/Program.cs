2e227909dcd18e43e6bea61a9d89336da42673f4
Colby McDonald <67612969+Colby-M@users.noreply.github.com>
2023-12-05 11:43:53 -0500

    Finished
    Got it all working and returning correct distances, might try to export csv

--- AlgorithmsDotNet/Program.cs
+++ AlgorithmsDotNet/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Algorithms
+﻿using System.Collections;
+
+namespace Algorithms
 {
     class AlgorithmsDotNet
     {
+        private static int iterations = 100000;
+        private static bool printEach = false;
+
         private static List<Node> nodes = new();
         private static int numberOfCapitals = 0;
+        private static List<int> bestOrder = new();
+        private static int bestTime = int.MaxValue;
         static void Main(string[] args)
         {
             if (args.Length < 1)
@@ -11,10 +18,10 @@
                 Console.Error.WriteLine("No Input File was given");
                 return;
             }
-            // Console.WriteLine(args[0]);
-            if (File.Exists(args[0]))
+
+            if (File.Exists(args.ElementAtOrDefault(0)))
             {
-                string[] lines = File.ReadAllLines(args[0]);
+                string[] lines = File.ReadAllLines(args.ElementAtOrDefault(0) ?? "");
                 int.TryParse(lines.ElementAtOrDefault(0), out numberOfCapitals);
                 
                 for (int i = 1; i < lines.Length - 1; i++)
@@ -30,16 +37,64 @@
                 }
 
                 // have all nodes into list with custom objects
-                // start doing the randomized nodes and saving minimum
-                // to make sure it doesn't go to one already visited
-                // make a list of all capitals, 0-numberOfCapitals
-                // randomize from this list
+                // loop through this n times and print out each order and time
+                for (int i = 0; i < iterations; i++)
+                {
+                    (List<int> order, int time) = findTime();
+                    if (printEach)
+                    {
+                        Console.WriteLine($"{string.Join(',', order)}  {time}");
+                    }
+                    if (time < bestTime)
+                    {
+                        bestTime = time;
+                        bestOrder = order;
+                    }
+                }
+
+                Console.WriteLine($"Best Order found: {string.Join(',', bestOrder)}");
+                Console.WriteLine($"Best Time found: {bestTime}");
             }
             else
             {
-                Console.Error.WriteLine($"Unable to open file: {args[0]}");
+                Console.Error.WriteLine($"Unable to open file: {args.ElementAtOrDefault(0)}");
             }
             return;
+        }
+
+        static (List<int> order, int time) findTime()
+        {
+            List<int> order = new();
+            int time = 0;
+            // get the simple array, 0 to number of Capitals - 1
+            for (int i = 0; i < numberOfCapitals; i++)
+            {
+                order.Add(i);
+            }
+            // randomize array
+            shuffle(ref order);
+            for (int i = 0; i < order.Count - 1; i++)
+            {
+                int source = order.ElementAtOrDefault(i);
+                int destination = order.ElementAtOrDefault(i + 1);
+                Node currentNode = nodes.FirstOrDefault(x => x.Source == source && x.Destination == destination)
+                    ?? throw new ArgumentException("Error finding Node");
+                time += currentNode.Weight;
+            }
+            return (order, time);
+        }
+
+        public static void shuffle(ref List<int> array)
+        {
+            Random rng = new();
+            int n = array.Count;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                int temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
         }
     }
 
@@ -55,10 +110,10 @@
             string[] numbers = line.Split(' ');
             int.TryParse(numbers.ElementAtOrDefault(0), out int s);
             this.Source = s;
-            int.TryParse(numbers.ElementAtOrDefault(0), out int d);
-            this.Source = d;
-            int.TryParse(numbers.ElementAtOrDefault(0), out int w);
-            this.Source = w;
+            int.TryParse(numbers.ElementAtOrDefault(1), out int d);
+            this.Destination = d;
+            int.TryParse(numbers.ElementAtOrDefault(2), out int w);
+            this.Weight = w;
         }
     }
 }
